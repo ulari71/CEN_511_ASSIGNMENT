d1935d2c9f9558f6308adb62b4ed9efe6e439d43
AMECHI ULARI <78308318+ulari71@users.noreply.github.com>
2021-02-01 00:13:30 +0100

    ADDED ARDUINO FILE

---
+++ sketch_jan31a.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("My name is Amechi Ulari and my matric number is 16cj020696");
+  delay(3000);
+}
